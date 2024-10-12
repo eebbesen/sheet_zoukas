37ef152839b987254852de7412e1c79be567be00
eebbesen <eebbesen.git@gmail.com>
2024-10-12 13:57:06 -0500

    comment out failing test

--- spec/sheet_zoukas_spec.rb
+++ spec/sheet_zoukas_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe SheetZoukas do
     expect(SheetZoukas::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
+  # it 'does something useful' do
+  #   expect(false).to eq(true)
+  # end
 end
